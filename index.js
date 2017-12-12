5472e24adac1ad3b585b4aa505a6e75522538739
Rubens Mariuzzo <rubens@mariuzzo.com>
2017-12-12 10:35:04 -0400

    Added code comments.

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const debug = require('debug')('fixture-middleware')
 
 module.exports = fixtureMiddleware
 
+/**
+ * Create a fixture middleware.
+ * @param {String} directory The directory containing fixtures.
+ * @return {Function}
+ */
+
 function fixtureMiddleware(directory) {
 
   debug('fixture directory:', directory)
@@ -34,29 +40,43 @@ function fixtureMiddleware(directory) {
     throw new TypeError('directory path must be a string')
   }
 
+  /**
+   * Fixture Middleware.
+   * @param {Object} req The request.
+   * @param {Object} res The response.
+   * @param {Function} next The next function.
+   */
   return async function fixtureMiddleware(req, res, next) {
+
     debug('request url:', req.url)
 
+    // Ignore any non-get request.
     if (req.method !== 'GET') {
-      debug('ignoring non-GET request')
+      debug('ignoring non-GET request:', req.method)
       return next()
     }
 
+    // Determine the name of the fixture file.
+    // Example: /user/all/active > user--all--active
     const fixtureName = req.url.substr(1).replace(/\//g, '--')
 
-    debug('fixture name:', fixtureName)
+    debug('requested fixture name:', fixtureName)
 
     const fixtureBasePath = path.join(directory, fixtureName)
     const fixturePaths = glob.sync(`${fixtureBasePath}.*`)
 
     let [fixturePath] = fixturePaths
+
+    // If request accept header is present, then we use it to determine the file type.
     if (req.headers.accept) {
       debug('using request accept headers:', req.headers.accept)
       const extension = mime.extension(req.headers.accept)
+
       debug('extension from request accept headers:', extension)
       fixturePath = fixturePaths.find(fp => fp.endsWith(`.${extension}`))
     }
 
+    // When no fixture file is found, then delegate request to next middleware in chain.
     if (!fixturePath) {
       debug('no fixture found')
       return next()
@@ -64,6 +84,7 @@ function fixtureMiddleware(directory) {
 
     debug('matching fixture:', fixturePath)
 
+    // Read the fixture file and return it.
     try {
       await fs.access(fixturePath)
       const stat = await fs.stat(fixturePath)
